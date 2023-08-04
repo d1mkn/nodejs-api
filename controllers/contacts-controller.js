00791b04c9f3211cc89f68acca7271b903f5f99f
Dmytro Hubin <dimkanishe2099@gmail.com>
2023-08-04 05:10:06 +0300

    hw 03

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -1,29 +1,30 @@
-import contactsService from "../models/contacts.js";
+import Contact from "../models/contact.js";
 import ctrlWrapper from "../decorators/ctrlWrapper.js";
 import HttpError from "../helpers/HttpError.js";
 
 const getAll = async (_, res) => {
-  const result = await contactsService.listContacts();
+  const result = await Contact.find();
   res.json(result);
 };
 
 const getById = async (req, res) => {
   const { id } = req.params;
-  const result = await contactsService.getContactById(id);
+  const result = await Contact.findById(id);
   if (!result) {
-    throw HttpError(404, "Not found");
+    throw HttpError(404, "Not Found");
   }
   res.json(result);
 };
 
 const add = async (req, res, next) => {
-  const result = await contactsService.addContact(req.body);
+  const result = await Contact.create(req.body);
+  console.log(req);
   res.status(201).json(result);
 };
 
 const deleteById = async (req, res, next) => {
   const { id } = req.params;
-  const result = await contactsService.removeContact(id);
+  const result = await Contact.findByIdAndRemove(id);
   if (!result) {
     throw HttpError(404, "Not found");
   }
@@ -34,12 +35,19 @@ const deleteById = async (req, res, next) => {
 
 const updateById = async (req, res, next) => {
   const { id } = req.params;
-  console.log(id);
-  const result = await contactsService.updateContact(id, req.body);
-  console.log(result);
-  if (!result) {
-    throw HttpError(404);
-  }
+  const result = await Contact.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+  if (!result) throw HttpError(404);
+  res.json(result);
+};
+
+const updateStatusById = async (req, res, next) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+  if (!result) throw HttpError(404);
   res.json(result);
 };
 
@@ -49,4 +57,5 @@ export default {
   add: ctrlWrapper(add),
   delete: ctrlWrapper(deleteById),
   updateById: ctrlWrapper(updateById),
+  updateStatusById: ctrlWrapper(updateStatusById),
 };
--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -1,6 +1,7 @@
 import express from "express";
 import contactsController from "../../controllers/contacts-controller.js";
 import isEmptyBody from "../../middlewares/isEmptyBody.js";
+import isValidId from "../../middlewares/isValidId.js";
 import validateBody from "../../decorators/validateBody.js";
 import contactsSchema from "../../schemas/contacts-schema.js";
 
@@ -8,7 +9,7 @@ const contactsRouter = express.Router();
 
 contactsRouter.get("/", contactsController.getAll);
 
-contactsRouter.get("/:id", contactsController.getById);
+contactsRouter.get("/:id", isValidId, contactsController.getById);
 
 contactsRouter.post(
   "/",
@@ -17,13 +18,21 @@ contactsRouter.post(
   contactsController.add
 );
 
-contactsRouter.delete("/:id", contactsController.delete);
+contactsRouter.delete("/:id", isValidId, contactsController.delete);
 
 contactsRouter.put(
   "/:id",
+  isValidId,
   isEmptyBody,
   validateBody(contactsSchema.contactUpdateSchema),
   contactsController.updateById
+);
+
+contactsRouter.patch(
+  "/:id/favorite",
+  isValidId,
+  validateBody(contactsSchema.contactUpdateStatus),
+  contactsController.updateStatusById
 );
 
 export default contactsRouter;
