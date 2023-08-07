1cb8d24c048ca3519fb195bd06ab58f0b6b33f49
Dmytro Hubin <dimkanishe2099@gmail.com>
2023-08-07 22:03:07 +0300

    added check for a token in the header

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -9,7 +9,7 @@ const { JWT_SECRET } = process.env;
 const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
-  if (bearer !== "Bearer") return next(HttpError(401, "Not authorized"));
+  if (bearer !== "Bearer" || !token) return next(HttpError(401, "Not authorized"));
   try {
     const { id } = jwt.verify(token, JWT_SECRET);
     const user = await User.findById(id);
